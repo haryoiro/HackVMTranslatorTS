67bfb8375fba32bfdb33107003af210baafaf023
haryoiro <mizusecocolte@gmail.com>
2021-03-19 09:42:03 +0900

    Refactoring

--- src/utils.ts
+++ src/utils.ts
@@ -8,23 +8,22 @@ export const expandAllFiles = (
   dir: string,
   pathArr: string[]
 ): string[] => {
-
   // ディレクトリ内のすべての項目を取得
-  fs.readdirSync(dir).forEach((filename) => {
+  const allFiles = fs.readdirSync(dir)
+  allFiles.forEach((filename) => {
       const fullPath = path.join(dir, filename)
+
       const stats = fs.statSync(fullPath)
 
       // .vmファイルのみ抽出
       if (stats.isFile() && isVM(fullPath)) {
-        // 拡張子.vmのみ返す。
         pathArr.push(fullPath)
       }
+
       // ディレクトリがなくなるまで再帰的に実行
       else if (stats.isDirectory()) {
         pathArr.push(...expandAllFiles(fullPath, pathArr))
       }
-
     })
-
   return pathArr
 }
